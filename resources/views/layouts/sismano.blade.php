20d89160bb77150d015d612b98ff812691458f1b
locasmendes <lukinhazmm@gmail.com>
2020-11-23 11:05:44 -0300

    corrigindo importações de css/js

--- resources/views/layouts/sismano.blade.php
+++ resources/views/layouts/sismano.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SiSMaNo</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
     @stack('styles')
 </head>
 <body>
 @yield('body')
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ url('js/app.js') }}"></script>
 @stack('scripts')
 </body>
 </html>
