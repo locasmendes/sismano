7d53468b6c835e30739a9c301f46eececbe0b5c1
locasmendes <lukinhazmm@gmail.com>
2020-12-08 16:58:14 -0300

    adicionando texto introdutório

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,10 @@
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-6">
-        <h1>SiSMaNo</h1>
-        <h3>Sistema de Manipulação de Notas</h3>
+        <h3> <b>SiSMaNo</b> - <b>Sis</b>tema de <b>Ma</b>nipulação de <b>No</b>tas</h3>
+        <p>
+            Este é um sistema com aplicações escolares com o intuito de facilitar o trabalho dos professores quanto ao controle e manipulação de notas dos alunos.
+        </p>
     </div>
 </div>
 @endsection
