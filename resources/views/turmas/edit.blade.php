939e5e61d2158499f6ec542db3e49c15e4d09a59
locasmendes <lukinhazmm@gmail.com>
2020-12-08 17:06:10 -0300

    corrigindo texto

--- resources/views/turmas/edit.blade.php
+++ resources/views/turmas/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Edit Product</h2>
+                <h2>Editar Disciplina</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('turmas.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
